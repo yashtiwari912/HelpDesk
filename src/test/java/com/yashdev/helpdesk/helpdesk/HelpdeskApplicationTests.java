0bf8ecae4dcb07625e68c27b3d9c8e57c575cc81
yashtiwari912 <yashtiwari92003@gmail.com>
2026-07-28 15:52:37 +0530

    Intialize Project

---
+++ src/test/java/com/yashdev/helpdesk/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashdev.helpdesk.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
